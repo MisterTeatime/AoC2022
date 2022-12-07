077514881b9e6f02e09368c9395d3626b845dc2d
Christoph Werner <christoph.werner@roedl-dynamics.com>
2022-12-07 11:23:27 +0100

    Tag 07 Überlegungen

--- docs/Day07.md
+++ docs/Day07.md
@@ -2,6 +2,18 @@
 
 ## Überlegungen
 
+- Data Class, um ein FileObject darzustellen
+  - name: Bezeichnung des Objekts
+  - size: Größe des Objekts (kann auch 0 sein. s. `getSize()`)
+  - content: Objekte in diesem Objekt
+  - Dateien haben keinen Inhalt
+  - Verzeichnisse werden mit size = 0 initialisiert
+  - `getSize()`
+    - Gibt *size* zurück, wenn es keinen Inhalt gibt (dann ist das Objekt eine Datei)
+    - Gibt die Summe aller `getSize()` Aufrufe der Inhaltsobjekte zurück
+- Vielleicht eine Liste alle angelegten Verzeichnisse für den direkten Zugriff?
+
+
 ## Teil 1
 
 ## Teil 2
--- src/Day07.kt
+++ src/Day07.kt
@@ -16,3 +16,52 @@ fun main() {
     println(part1(input))
     println(part2(input))
 }
+
+fun dirDemo() {
+    
+    val directory = FileObject("/")
+    println(directory.getSize())
+
+    val a = directory.addContent("a")
+    directory.addContent("b.txt", 14848514)
+    directory.addContent("c.dat", 8504156)
+
+    val d = directory.addContent("d")
+
+    val e = a.addContent("e")
+    a.addContent("f", 29116)
+    a.addContent("g", 2557)
+    a.addContent("h.lst", 62596)
+
+    e.addContent("i", 584)
+
+    d.addContent("j", 4060174)
+    d.addContent("d.log", 8033020)
+    d.addContent("d.ext", 5626152)
+    d.addContent("k", 7214296)
+
+    println(directory.getSize())
+}
+
+data class FileObject(val name: String, val content: MutableList<FileObject> = mutableListOf()) {
+    private var size: Int = 0
+
+    constructor(name: String, size: Int, content: MutableList<FileObject> = mutableListOf()) : this(name, content) {
+        this.size = size
+    }
+
+    fun getSize(): Int = when {
+        content.isEmpty() -> size
+        else -> content.sumOf { it.getSize() }
+    }
+
+    fun addContent(item: FileObject) = content.add(item)
+
+    fun addContent(name: String, size: Int) = addContent(FileObject(name, size))
+
+    fun addContent(name: String): FileObject {
+        val item = FileObject(name)
+        addContent(item)
+        return item
+    }
+}
