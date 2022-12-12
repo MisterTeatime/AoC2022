219520bbc808e32a780b6656ecaabfb96f929a1d
MisterTeatime <j.teatime@gmail.com>
2022-12-12 01:48:40 +0100

    Tag 07

--- docs/Day07.md
+++ docs/Day07.md
@@ -2,18 +2,44 @@
 
 ## Überlegungen
 
-- Data Class, um ein FileObject darzustellen
-  - name: Bezeichnung des Objekts
-  - size: Größe des Objekts (kann auch 0 sein. s. `getSize()`)
-  - content: Objekte in diesem Objekt
-  - Dateien haben keinen Inhalt
-  - Verzeichnisse werden mit size = 0 initialisiert
-  - `getSize()`
-    - Gibt *size* zurück, wenn es keinen Inhalt gibt (dann ist das Objekt eine Datei)
-    - Gibt die Summe aller `getSize()` Aufrufe der Inhaltsobjekte zurück
-- Vielleicht eine Liste alle angelegten Verzeichnisse für den direkten Zugriff?
+### Data Class TreeNode
 
+Die Datenklasse repräsentiert einen Knoten im Dateisystem. Jeder Knoten besitzt einen Namen und hat einen Inhalt. Über einen sekundären Konstruktor kann die Größe mit angegeben werden (nur bei Dateien erforderlich). Es gibt auch noch einen Eigenschaft für den Elternknoten.
+
+Die Größe wird über `getSize()` abgefragt und entspricht entweder dem hinterlegten Wert (bei Dateien) oder wird berechnet, in dem die Größen der Knoten im Inhalt aufsummiert werden.
+
+Die Klasse bietet zudem Methoden, um Inhalt hinzuzufügen (`addChild()`) und ein Kindknoten aus dem Inhalt abzurufen. Beim Hinzufügen wird der Knoten, dessen Methode aufgerufen wurde, als *parent* dem neuen Kind zugeordnet.
+
+### Parsen des Verzeichnisbaums
+
+Beim Parsen wird die Log der Eingabeaufforderung durchgegangen. Zu Beginn wird der Root-Knoten angelegt, als *currentNode* festgelegt und dem Set der bekannten Verzeichnisse hinzugefügt.
+
+Die restlichen Zeilen (ohne die ersten beiden) werden der Reihe nach verarbeitet:
+
+- Die Zeilen werden am Leerzeichen aufgesplittet
+- Ist der erste String eine Zahl, dann wird *currentElement* ein neuer Knoten hinzugefügt. Da Name (zweiter String) und Größe (erster String) angegeben werden, handelt es sich um eine Datei
+- Ist der erste String `dir`, dann ist es ein Verzeichnis. *currentElement* erhält einen neuen Knoten, der nur aus dem Namen (zweiter String) besteht
+- Ist der erste String `$` und der zweite String `cd`, dann kommt es auf den dritten String an
+  - Ist er `..`, dann wird der *parent* von *currentNode* die neue *currentNode*
+  - Sonst wird *currentNode* auf die TreeNode aus dem Inhalt von *currentNode* gesetzt, die den entsprechenden Namen (drtter String) hat
+  - `$ ls` wird schlicht ignoriert, da die Anweisung für das Parsen keine Bedeutung hat
+
+Nach dem Parsen ist der Wurzelknoten bekannt und alle Knoten sind entsprechend enthalten. *nodes* ist ein Set mit allen Verzeichnissen, da für die Aufgaben verwendet werden kann.
 
 ## Teil 1
 
+Es wird die Summe der Größen aller Verzeichnisse gesucht, die höchstens eine Größe von 100.000 Bytes haben.
+
+Nach dem Parsen werden die Elemente von *nodes* auf ihre Größen gemappt, dann die Elemente behalten, die die Bedingung erfüllen (<= 100_000) und dann werden die Größen aufsummiert.
+
+**Lösung**: 1743217
+
 ## Teil 2
+
+Für das Update werden 30_000_000 Bytes Platz benötigt. Der Gesamtspeicherplatz ist 70_000_000 Bytes. Falls nicht genug Platz zur Verfügung steht (was es tut), muss ein Verzeichnis gelöst werden. Der Kandidat ist das kleinste Verzeichnis, dass mindestens den fehlenden Platz freigibt. Gesucht ist die Größe dieses Verzeichnisses.
+
+Nach dem Parsen wird der Platz berechnet, der freigegeben werden muss. Platz für das Update - aktuell freier Platz. Sollte der Wert kleiner 0 sein, dann ist mehr Speicher frei als benötigt wird und es muss nichts gelöscht werden (wird nicht vorkommen).
+
+Die Elemente von *nodes* werden wieder auf ihre Grö´ße gemappt und auch wieder gefiltert. Diesmal werden alle Verzeichnisse behalten, die größer oder gleich dem Platz sind, der freigegeben werden muss. Aus dieser Gruppe wird dann das kleinste Elemente zurückgegeben.
+
+**Lösung**: 8319096
--- src/Day07.kt
+++ src/Day07.kt
@@ -1,52 +1,79 @@
 fun main() {
+    val totalSpace = 70_000_000
+    val updateSpace = 30_000_000
+
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val nodes = mutableSetOf<TreeNode>()
+
+        val root = TreeNode("/")
+        var currentNode = root
+        nodes.add(root)
+
+        //Verzeichnisbaum erstellen
+        input.drop(2).map { it.split(" ") }
+            .forEach {item ->
+                when {
+                    item[0].matches("""\d+""".toRegex()) -> currentNode.addChild(item[1], item[0].toInt())
+                    item[0] == "dir" -> nodes.add(currentNode.addChild(item[1]))
+                    item[0] == "$" && item[1] == "cd" -> currentNode = when (item[2]) {
+                        ".." -> currentNode.parent!!
+                        else -> currentNode.getContent(item[2])
+                    }
+                }
+            }
+
+        //Ergebnis suchen
+        return nodes.map { it.getSize() }.filter { it <= 100_000 }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
 
+        val nodes = mutableSetOf<TreeNode>()
+
+        val root = TreeNode("/")
+        var currentNode = root
+        nodes.add(root)
+
+        //Verzeichnisbaum erstellen
+        input.drop(2).map { it.split(" ") }
+            .forEach {item ->
+                when {
+                    item[0].matches("""\d+""".toRegex()) -> currentNode.addChild(item[1], item[0].toInt())
+                    item[0] == "dir" -> nodes.add(currentNode.addChild(item[1]))
+                    item[0] == "$" && item[1] == "cd" -> currentNode = when (item[2]) {
+                        ".." -> currentNode.parent!!
+                        else -> currentNode.getContent(item[2])
+                    }
+                }
+            }
+
+        //Ergebnis suchen
+        val spaceToFreeUp = updateSpace - (totalSpace - root.getSize())
+
+        if (spaceToFreeUp <= 0) return 0
+
+        return nodes
+            .map { it.getSize() }
+            .filter { it >= spaceToFreeUp }
+            .minOf { it }
+    }
+    
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
-    check(part1(testInput) == 1)
-    check(part2(testInput) == 1)
+    check(part1(testInput) == 95437)
+    check(part2(testInput) == 24933642)
 
     val input = readInput("Day07")
     println(part1(input))
     println(part2(input))
 }
 
-fun dirDemo() {
-    
-    val directory = FileObject("/")
-    println(directory.getSize())
-
-    val a = directory.addContent("a")
-    directory.addContent("b.txt", 14848514)
-    directory.addContent("c.dat", 8504156)
-
-    val d = directory.addContent("d")
-
-    val e = a.addContent("e")
-    a.addContent("f", 29116)
-    a.addContent("g", 2557)
-    a.addContent("h.lst", 62596)
-
-    e.addContent("i", 584)
-
-    d.addContent("j", 4060174)
-    d.addContent("d.log", 8033020)
-    d.addContent("d.ext", 5626152)
-    d.addContent("k", 7214296)
-
-    println(directory.getSize())
-}
-
-data class FileObject(val name: String, val content: MutableList<FileObject> = mutableListOf()) {
+data class TreeNode(val name: String, val content: MutableList<TreeNode> = mutableListOf()) {
     private var size: Int = 0
+    var parent: TreeNode? = null
 
-    constructor(name: String, size: Int, content: MutableList<FileObject> = mutableListOf()) : this(name, content) {
+    constructor(name: String, size: Int, content: MutableList<TreeNode> = mutableListOf()) : this(name, content) {
         this.size = size
     }
 
@@ -55,13 +82,18 @@ data class FileObject(val name: String, val content: MutableList<FileObject> = m
         else -> content.sumOf { it.getSize() }
     }
 
-    fun addContent(item: FileObject) = content.add(item)
+    private fun addChild(item: TreeNode) {
+        content.add(item)
+        item.parent = this
+    }
 
-    fun addContent(name: String, size: Int) = addContent(FileObject(name, size))
+    fun addChild(name: String, size: Int) = addChild(TreeNode(name, size))
 
-    fun addContent(name: String): FileObject {
-        val item = FileObject(name)
-        addContent(item)
+    fun addChild(name: String): TreeNode {
+        val item = TreeNode(name)
+        addChild(item)
         return item
     }
+
+    fun getContent(name: String) = this.content.first { it.name == name }
 }
