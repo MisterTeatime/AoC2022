477ca4a11caa664490f84a54387be7ee7fb1fa4e
MisterTeatime <j.teatime@gmail.com>
2022-12-21 12:34:54 +0100

    Extensions restrukturiert

--- src/Utils.kt
+++ src/Utils.kt
@@ -182,33 +182,3 @@ fun <T> List<T>.partitionGroups(divider: T): List<List<T>> {
         result.add(this.subList(startIndex, this.size))
     return result
 }
-
-/**
- * Sucht aus [collection] die [n] größten Elemente und gibt sie als Liste zurück.
- */
-fun <T: Comparable<T>> topMax(n: Int, collection: Iterable<T>): List<T> {
-    return collection.fold(mutableListOf<T>()) { topList, candidate ->
-        if (topList.size < n || candidate > topList.last()) {
-            topList.add(candidate)
-            topList.sortDescending()
-            if (topList.size > n)
-                topList.removeAt(n)
-        }
-        topList
-    }
-}
-
-/**
- * Sucht aus [collection] die [n] kleinsten Elemente und gibt sie als Liste zurück.
- */
-fun <T: Comparable<T>> topMin(n: Int, collection: Iterable<T>): List<T> {
-    return collection.fold(mutableListOf<T>()) { topList, candidate ->
-        if (topList.size < n || candidate < topList.last()) {
-            topList.add(candidate)
-            topList.sort()
-            if (topList.size > n)
-                topList.removeAt(n)
-        }
-        topList
-    }
-}
