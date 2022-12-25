9af5ba19094fa46e119c9da3f803272934be4771
MisterTeatime <j.teatime@gmail.com>
2022-12-25 12:30:52 +0100

    Tag 25: Überlegungen

---
+++ docs/Day25.md
@@ -0,0 +1,5 @@
+# [Tag 25: Full of Hot Air](https://adventofcode.com/2022/day/25)
+
+## Teil 1
+
+## Teil 2
---
+++ src/Day25.kt
@@ -0,0 +1,18 @@
+fun main() {
+    fun part1(input: List<String>): String {
+        return ""
+    }
+
+    fun part2(input: List<String>): String {
+        return "input.size"
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day25_test")
+    check(part1(testInput) == "2=-1=0")
+    check(part2(testInput) == "1")
+
+    val input = readInput("Day25")
+    println(part1(input))
+    println(part2(input))
+}
